c9edd7abec4d0f360e48dad9baef8644ab3c92ec
Saúl Camacho <electrohead1200@gmail.com>
2023-09-18 10:47:15 -0600

    Mostrando Precios y Categorias en la vista Crear

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,6 @@
+import Precio from '../models/Precio.js';
+import Categoria from '../models/Categoria.js';
+
 const admin = (req, res) => {
     res.render('propiedades/admin', {
         pagina: 'Mis propiedades',
@@ -6,10 +9,18 @@ const admin = (req, res) => {
 }
 
 // Formulario para crear propiedades
-const crear = (req, res) => {
+const crear = async (req, res) => {
+    // Consultar Modelo de Precio y Categorias
+    const [categorias, precios] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll()
+    ])
+
     res.render('propiedades/crear', {
         pagina: 'Crear Propiedad',
-        barra: true
+        barra: true,
+        categorias,
+        precios
     })
 }
 
