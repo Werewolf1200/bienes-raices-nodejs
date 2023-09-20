173bd64633fb0e251729eb26bb106db3a8e75714
Saúl Camacho <electrohead1200@gmail.com>
2023-09-20 11:01:34 -0600

    Añadiendo CSRF y Validación al Formulario

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,4 @@
+import { validationResult } from 'express-validator';
 import Precio from '../models/Precio.js';
 import Categoria from '../models/Categoria.js';
 
@@ -19,12 +20,36 @@ const crear = async (req, res) => {
     res.render('propiedades/crear', {
         pagina: 'Crear Propiedad',
         barra: true,
+        csrfToken: req.csrfToken(),
         categorias,
         precios
     })
 }
 
+const guardar = async (req, res) => {
+    // validación
+    let resultado = validationResult(req);
+
+    if (!resultado.isEmpty()) {
+
+        const [categorias, precios] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll()
+        ])
+
+        return res.render('propiedades/crear', {
+            pagina: 'Crear Propiedad',
+            barra: true,
+            csrfToken: req.csrfToken(),
+            categorias,
+            precios,
+            errores: resultado.array()
+        })
+    }
+}
+
 export {
     admin,
-    crear
+    crear,
+    guardar
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { admin, crear } from '../controllers/propiedadController.js';
+import { admin, crear, guardar } from '../controllers/propiedadController.js';
+import { body } from "express-validator";
 
 const router = express.Router();
 
 router.get('/mis-propiedades', admin);
 router.get('/propiedades/crear', crear)
+router.post('/propiedades/crear',
+    body('titulo').notEmpty().withMessage('El Titulo del Anuncio es Obligatorio'),
+    body('descripcion')
+        .notEmpty().withMessage('La Descripción no puede ir vacia')
+        .isLength({ max: 200 }).withMessage('La Descripcion es muy larga'),
+    body('categoria').isNumeric().withMessage('Selecciona una Categoría'),
+    body('precio').isNumeric().withMessage('Selecciona un Rango de Precios'),
+    body('habitaciones').isNumeric().withMessage('Selecciona la Cantidad de Habitaciones'),
+    body('estacionamiento').isNumeric().withMessage('Selecciona la Cantidad de Estacionamientos'),
+    body('wc').isNumeric().withMessage('Selecciona la Cantidad de Baños'),
+    body('lat').notEmpty().withMessage('Ubica la Propiedad en el Mapa'),
+    guardar)
 
 export default router;
