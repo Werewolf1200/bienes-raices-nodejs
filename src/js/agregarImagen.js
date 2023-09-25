fd919af234eae5e34533f3c67ab17f3f084dcdd2
Saúl Camacho <electrohead1200@gmail.com>
2023-09-25 09:40:07 -0600

    Configurando Dropzone y añadiendole  proteccion CSRFT

--- src/js/agregarImagen.js
+++ src/js/agregarImagen.js
@@ -1,1 +1,18 @@
 import { Dropzone } from "dropzone";
+
+const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+Dropzone.options.imagen = {
+    dictDefaultMessage: 'Sube tus imágenes aqui',
+    acceptedFiles: '.png, .jpg, .jpeg',
+    maxFilesize: 5,
+    maxFiles: 1,
+    parallelUploads: 1,
+    autoProcessQueue: false,
+    addRemoveLinks: true,
+    dictRemoveFile: 'Borrar Archivo',
+    dictMaxFilesExceeded: 'El limite es 1 Archivo',
+    headers: { // Se ejecuta al principio
+        'CSRF-Token': token
+    }
+}
